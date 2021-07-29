c870844788c7c075c25b74079b980834a4a0651a
Cris Babalau <babalau99@mail.ru>
2021-07-30 00:44:02 +0800

    Упссс

--- project_folder/main.js
+++ project_folder/main.js
@@ -2,12 +2,12 @@ let money, income, addExpenses, deposit, mission, period;
 
 let randArr = ['money','income', 'addExpenses', 'deposit','mission','period'];
 
-var spans = document.getElementsByClassName("varName");
+let spans = document.getElementsByClassName("varName");
 
 for ( let i = 0; i < randArr.length; i++){
     spans[i].innerHTML = randArr[i];
 }
 
-alert("success")
+alert("success");
 
 console.log(Symbol(4));
