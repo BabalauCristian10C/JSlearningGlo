3a69fdd3c7f79b02592c0ac86629efffcbe249be
Cris Babalau <babalau99@mail.ru>
2021-07-30 19:36:27 +0800

    1.2 Доп задание

--- project_folder/main.js
+++ project_folder/main.js
@@ -2,7 +2,7 @@ let num = 266219;
 let numStr = num.toString();
 let product = parseInt(numStr[0], 10);
 let numArr = numStr.split('');
-for (let i=0; i< numStr.length; i++){
+for (let i=1; i< numStr.length; i++){
     num = parseInt(numArr[i], 10);
     product *= num;
 }
