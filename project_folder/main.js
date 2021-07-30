0cf75a7ea5e0959850fcf8415cb821cfb89c69a1
Cris Babalau <babalau99@mail.ru>
2021-07-30 17:42:31 +0800

    Второй урок _ Первая попытка

--- project_folder/main.js
+++ project_folder/main.js
@@ -1,13 +1,10 @@
-let money, income, addExpenses, deposit, mission, period;
-
-let randArr = ['money','income', 'addExpenses', 'deposit','mission','period'];
-
-let spans = document.getElementsByClassName("varName");
-
-for ( let i = 0; i < randArr.length; i++){
-    spans[i].innerHTML = randArr[i];
-}
-
-alert("success");
-
-console.log(Symbol(4));
+let money = 15000, income = "Stock Market", addExpenses = "Теннис, Курсы, Еда", deposit = true, mission = 30000;
+let period = 1 , budgetDay = money/30;
+let randArr = [money,income,addExpenses,deposit,mission,period];
+//              0      1        2           3         4   5
+//with convenience purposes
+console.log(typeof randArr[0] + ' ' + typeof randArr[1] + ' ' + typeof randArr[3]);
+console.log(addExpenses.length);
+console.log("Период равен " + period + " месяцу \n"  + "Цель заработать " + mission + " колумбийских песо");
+console.log(addExpenses.toLowerCase().split(', '));
+console.log(budgetDay);
