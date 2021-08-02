36f9f0fdf6932efe7709b2ae46641d4abaf184a2
Cris Babalau <babalau99@mail.ru>
2021-08-02 18:42:44 +0800

    Третий урок

--- project_folder/main.js
+++ project_folder/main.js
@@ -1,13 +1,33 @@
-let money, income, addExpenses, deposit, mission, period;
+let money, income = "Stock Market", addExpenses, deposit, mission = 45321,period;
+let q1,a1,q2,a2;
 
-let randArr = ['money','income', 'addExpenses', 'deposit','mission','period'];
+money = +prompt("Ваш месячный доход?", '0');
+addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую");
+deposit = confirm("Есть ли у вас депозит в банке?");
+q1 = prompt("Введите обязательную статью расходов?");
+a1 = parseInt(prompt("Во сколько это обойдется?"), 10);
+q2 = prompt("Введите обязательную статью расходов?");
+a2 = parseInt(prompt("Во сколько это обойдется?"), 10);
+let budgetMonth = money - a1 - a2;
+let budgetDay = Math.floor(budgetMonth/30);
+period = Math.ceil(mission/budgetMonth);
+let randArr = [money,income,addExpenses,deposit,mission,period];
 
-let spans = document.getElementsByClassName("varName");
+console.log(typeof randArr[0] + ' ' + typeof randArr[1] + ' ' + typeof randArr[3]);
+console.log(addExpenses.length);
+console.log("Период равен " + period + " месяцу \n"  + "Цель заработать " + mission + " колумбийских песо");
+console.log(addExpenses.toLowerCase().split(', '));
+console.log("Бюджет на месяц: " + budgetMonth);
+console.log("Цель будет достигнута за: " + period);
+console.log("Бюджет на день: " + budgetDay);
 
-for ( let i = 0; i < randArr.length; i++){
-    spans[i].innerHTML = randArr[i];
+if (budgetDay > 1200) {
+    console.log("У вас высокий уровень дохода");
+} else if (budgetDay>600 && budgetDay<1200 ) {
+    console.log("У вас средний уровень дохода");
+} else if (budgetDay < 600 && budgetDay > 0) {
+    console.log("К сожалению у вас уровень дохода ниже среднего");
+} else {
+    console.log("что то пошло не так");
 }
 
-alert("success");
-
-console.log(Symbol(4));
