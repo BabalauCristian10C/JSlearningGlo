cfe0d9e11bf01f6d096831b1326fc8f497a6b593
Chris Babalau <chrisbabalau@Chriss-MacBook-Air.local>
2021-08-04 19:00:11 +0300

    First

--- project_folder/main.js
+++ project_folder/main.js
@@ -1,13 +1,86 @@
-let money, income, addExpenses, deposit, mission, period;
+'use strict';
 
-let randArr = ['money','income', 'addExpenses', 'deposit','mission','period'];
 
-let spans = document.getElementsByClassName("varName");
+let income = "Stock Market", 
+    mission = 45000,
+    listOfQuestions = [];
 
-for ( let i = 0; i < randArr.length; i++){
-    spans[i].innerHTML = randArr[i];
+function showTypeOf(variable){
+    return typeof variable;
 }
 
-alert("success");
+const getIncomeMonth = function(){
+    let moneyMonth;
+    do {
+        moneyMonth = parseInt(prompt("Ваш месячный доход?", '0'), 10 );
+    }
+    while(isNaN(moneyMonth));
+    return moneyMonth;
+}
 
-console.log(Symbol(4));
+const getExpensesMonth = function (){
+    let sum = 0;
+    let arrayIndex = 0;
+
+    for (let i = 0; i < 4; i++){
+        if (i % 2 === 0) { 
+            listOfQuestions[arrayIndex] = prompt("Введите обязательную статью расходов?");
+            arrayIndex++;
+        } else {
+            let answer = parseInt(prompt("Во сколько это обойдется?"), 10);
+            if (isNaN(answer) || typeof answer === "string" || !answer) {
+                --i;
+            } else {
+                sum += answer;
+            } 
+        }
+    }
+    return sum;
+};
+
+
+const getAccumulatedMonth = function (income, callback){
+    const rest = income - callback;
+    return rest;
+};
+
+const getTargetMonth = function (scope, accumulated){
+    const period = Math.ceil(scope/accumulated);
+    return period;
+};
+
+
+const getStatusIncome = function (budgetDay) {
+    if (budgetDay > 1200) {
+        return "У вас высокий уровень дохода";
+    } else if (budgetDay>600 && budgetDay<1200 ) {
+        return "У вас средний уровень дохода";
+    } else if (budgetDay < 600 && budgetDay > 0) {
+        return "К сожалению у вас уровень дохода ниже среднего";
+    } else {
+        return "что то пошло не так";
+    }
+};
+
+//month income
+let money = getIncomeMonth();
+let addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую");
+let deposit = confirm("Есть ли у вас депозит в банке?");
+let monthExpenses = getExpensesMonth();
+let accumulatedMonth = getAccumulatedMonth(money, monthExpenses);
+let period = getTargetMonth(mission, accumulatedMonth);
+let budgetDay = Math.floor(accumulatedMonth/30);
+
+
+console.log(showTypeOf(money) + ' ' + showTypeOf(income) + ' ' + showTypeOf(deposit));
+console.log(monthExpenses);
+console.log(addExpenses.toLowerCase().split(', '));
+
+if (period > 0 ){
+    console.log("Цель будет достигнута через " + period + " месяцу / месяцев");
+} else {
+    console.log("Цель не будет достигнут ");
+}
+
+console.log("Бюджет на день: " + budgetDay);
+console.log(getStatusIncome(budgetDay));
