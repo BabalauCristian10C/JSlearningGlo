e4923f0ec665b0c8f6a4792cb97bf4777b0b0572
Chris Babalau <chrisbabalau@Chriss-MacBook-Air.local>
2021-08-21 02:43:39 +0300

    first part

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const calculate = document.querySelector('#start'),
     plusButt = document.querySelectorAll(".btn_plus"),
     incomePlus = plusButtons[0],
     expensesPlus = plusButtons[1],
-    checkBox = document.getElementById("deposit-check"),
     addIncomeItem = document.querySelectorAll(".additional_income-item"),
     budgetDayValue = document.getElementsByClassName('budget_day-value')[0],
     expensesMonthValue = document.getElementsByClassName('expenses_month-value')[0],
@@ -24,6 +23,7 @@ const calculate = document.querySelector('#start'),
     depositCheck = document.querySelector("#deposit-check"),
     selector = document.querySelector(".deposit-bank"),
     depositAmount = document.querySelector(".deposit-amount"),
+    depositPercent = document.querySelector(".deposit-percent"),
     targetAmount = document.querySelector(".target-amount"),
     range = document.querySelector('[type="range"]'),
     periodSelect = document.querySelector(".period-select"),
@@ -60,6 +60,7 @@ class AppData{
         this.budgetMonth = 0; 
         this.expensesMonth = 0; 
         this.moneyPeriod = 0;
+        this.passThrew = false;
     }
 
     start(){
@@ -86,6 +87,7 @@ class AppData{
         appData.getExpInc();
         appData.getExpensesIncomesMonth();
         appData.inputExpenseTitle();
+        appData.getInfoDeposit();
         appData.getBudget();
         appData.getAddIncomeItem();
         appData.inputIncomeTitle();
@@ -158,7 +160,8 @@ class AppData{
     }
     
     getBudget() {
-        this.budgetMonth = parseInt(this.budget, 10) + this.incomeMonth - appData.expensesMonth;
+        const monthDeposit = parseInt(this.moneyDeposit,10) * (parseInt(this.percentDeposit,10) / 100);
+        this.budgetMonth = parseInt(this.budget, 10) + this.incomeMonth - appData.expensesMonth + parseInt(monthDeposit, 10);
         this.budgetDay = Math.floor(this.budgetMonth / 30); 
     }
     
@@ -275,6 +278,53 @@ class AppData{
     
     }
     
+    getInfoDeposit(){
+        if (this.deposit) {
+            if (this.passThrew === true){
+                console.log("succes");
+            } else {
+                this.percentDeposit = selector.value;
+            }
+            this.moneyDeposit = depositAmount.value;
+        }
+    }
+
+    changePercent(){
+        const selectIndex = this.value;
+        if (selectIndex === "other") {
+            appData.passThrew = true;
+            depositPercent.style.display = "inline-block";
+            depositPercent.value = "";
+            depositPercent.addEventListener("blur", () => {
+                if (depositPercent.value > 100 || depositPercent.value < 1){
+                    calculate.setAttribute("disabled", true);
+                } else {
+                    calculate.removeAttribute("disabled");
+                    appData.percentDeposit = depositPercent.value;
+                }
+            });
+        } else {
+            appData.passThrew = false;
+            depositPercent.style.display = "none";
+            depositPercent.value =  selectIndex;
+        }
+    }
+
+    depositHandler(){
+        if(depositCheck.checked){
+            selector.style.display = 'inline-block';
+            depositAmount.style.display = 'inline-block';
+            this.deposit = true;
+            selector.addEventListener('change', this.changePercent);
+        } else  {
+            selector.style.display = 'none';
+            depositAmount.style.display = 'none';
+            depositPercent.style.display = "none";
+            depositAmount.value = "";
+            selector.value = "";
+            selector.removeEventListener('change', this.changePercent);
+        }
+    }
     addEventListeners() {
         calculate.addEventListener('click', appData.start);
         plusButt.forEach((item) => item.addEventListener('click', appData.addExpIncBlock));
@@ -282,6 +332,8 @@ class AppData{
             appData.showInvest();
             document.getElementsByClassName("period-amount")[0].innerHTML = periodSelect.value;
         });
+
+        depositCheck.addEventListener("change", this.depositHandler.bind(this));
     }
 
 }
